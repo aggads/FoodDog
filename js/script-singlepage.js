a51bf1d281d8eae28fe5ebc4247ac3392c1779a8
MDE86 <michel.deraemaeker@gmail.com>
2018-11-15 21:34:04 +0100

    search function layout complete

--- js/script-singlepage.js
+++ js/script-singlepage.js
@@ -1,3 +1,4 @@
+//HAMBURGER MENU
 function hamburgerMenu() {
     let x = document.getElementById("header-menu-hamburger");
     if (x.style.display === "block") {
@@ -6,3 +7,33 @@ function hamburgerMenu() {
         x.style.display = "block";
     }
 }
+
+let hambMenu = document.getElementById("header-menu-hamburger")
+
+window.onclick = function(event) {
+    if (event.target == hambMenu) {
+        hambMenu.style.display = "none";
+    }
+}
+// ----------
+
+
+//SEARCH BUTTON & MODAL WINDOW
+let wholeWindow = document.getElementById('modalwindow');
+let btn = document.getElementById("button-search-id");
+let closeWindow = document.getElementsByClassName("search-close-window")[0]; 
+
+btn.onclick = function() {
+    wholeWindow.style.display = "block";
+}
+
+closeWindow.onclick = function() {
+    wholeWindow.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == wholeWindow) {
+        wholeWindow.style.display = "none";
+    }
+}
+// -----------
