62973fc32ed9e4957973e4478d89d7c86dc348c7
root <root@Crow.crow>
2018-11-28 08:02:31 +0000

    in progress

--- js/ajaxArticle.js
+++ js/ajaxArticle.js
@@ -1,16 +1,29 @@
-   var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://foodog.herokuapp.com/articles', true);
- xhr.onload = function() {
-      if(this.status == 200){
-      var article = JSON.parse(this.responseText);
-   //console.log(article.docs[0].title);
+   var lifestyle =document.getElementById("title_lifestyle").innerHTML;
+   var nutrition =document.getElementById("title_nutrition").innerHTML;
+   var community =document.getElementById("title_community").innerHTML;
+   var wellness =document.getElementById("title_wellness").innerHTML;
+   console.log(lifestyle);
 
- var output = '';
- 
-      for(let i in article.docs){
-        //console.log(article.docs[i]);
-        output +=
-        ` <article class="Article all_Articles">
+   var xhr = new XMLHttpRequest();
+   xhr.open('GET', 'https://foodog.herokuapp.com/articles', true);
+   xhr.onload = function() {
+       if (this.status == 200) {
+           var article = JSON.parse(this.responseText);
+           //console.log(article.docs[0].title);
+
+           for (let i in article.docs) {
+               var output = '';
+               var teste = article.docs[i].tagForArticle
+               console.log(teste)
+               for(let a in teste){
+
+               if (teste[a] === 'community' && community===community) {
+                var arr= teste[a]
+                   console.log(arr)
+               
+
+                   output +=
+                       ` <article class="Article all_Articles">
         <img alt="img" src="${article.docs[i].imgUrl}" width="500px" height="500px" />
         <div class="column">
         <div class="circle"></div>
@@ -24,9 +37,12 @@
       </article>
       <hr>
         `
+        document.getElementById('all_Articles').innerHTML = output;
 
-      }
-      document.getElementById('all_Articles').innerHTML = output;
-  }
- }
-  xhr.send();    
+                }
+               }   
+           }
+       }
+   }
+   xhr.send();
+
