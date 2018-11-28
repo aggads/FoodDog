0307a0e9c5108be54997a4e934757b929abd20bc
aggads <aggad.samy@gmail.com>
2018-11-28 12:27:30 +0100

    fix bug

--- js/script-foodog.js
+++ js/script-foodog.js
@@ -1,5 +1,5 @@
 let wholeWindow = document.getElementById('modalwindow');
-let btn = document.getElementById("button-search-id");
+let btn = document.getElementById("button-search-id logo-social-media1");
 let closeWindow = document.getElementsByClassName("search-close-window")[0]; 
 
 btn.onclick = function() {
@@ -15,3 +15,45 @@ window.onclick = function(event) {
         wholeWindow.style.display = "none";
     }
 }
+
+
+var close = 0;
+    function menu_mobile() {
+      if (close == 0) {
+        document.getElementById("container_menu_drop").classList.remove("hide1");
+        document.getElementById("container_menu_drop").classList.add("show");
+        document.getElementById("container_menu_drop").classList.remove("hide_Subjets");
+        document.getElementById("container_menu_drop").classList.add("show");
+        close++;
+      } else if (close == 1) {
+        document.getElementById("container_menu_drop").classList.remove("show");
+        document.getElementById("container_menu_drop").classList.add("hide1");
+        document.getElementById("container_menu_drop").classList.remove("show");
+        document.getElementById("container_menu_drop").classList.add("hide_Subjets");
+        close--;
+      }
+    }
+    function fixedHeader() {
+      document.getElementById("mobile_show").classList.remove("masked_loadpage");
+      document.getElementById("mobile_show").classList.add("show_loadpage");
+      document.getElementById("mobile_main").classList.add("masked_loadpage");
+    }
+    function change_post_best() {
+      document.getElementById("firstSect").classList.add("show");
+      document.getElementById("secondSect").classList.add("masked");
+      document.getElementById("thirdSect").classList.add("masked");
+      document.getElementById("secondSect").classList.remove("show");
+      document.getElementById("thirdSect").classList.remove("show");
+    } function change_post_Featured() {
+      document.getElementById("secondSect").classList.add("show");
+      document.getElementById("firstSect").classList.add("masked");
+      document.getElementById("thirdSect").classList.add("masked");
+      document.getElementById("firstSect").classList.remove("show");
+      document.getElementById("thirdSect").classList.remove("show");
+    } function change_post_Latest() {
+      document.getElementById("thirdSect").classList.add("show");
+      document.getElementById("secondSect").classList.add("masked");
+      document.getElementById("firstSect").classList.add("masked");
+      document.getElementById("firstSect").classList.remove("show");
+      document.getElementById("secondSect").classList.remove("show");
+    }
